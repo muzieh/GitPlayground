1066d1094140b8fdd43396ba7309bfffa4de7989
Muzieh <muzieh@gmail.com>
2013-11-20 22:19:56 +0000

    XNA project changed into MonoGame projec for proper Content processing

--- MonoGameTest/MonoGameTest/Properties/AssemblyInfo.cs
+++ MonoGameTest/MonoGameTest/Properties/AssemblyInfo.cs
@@ -5,9 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MonoTest")]
-[assembly: AssemblyProduct("MonoTest")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("MonoGameTest")]
+[assembly: AssemblyProduct("MonoGameTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
@@ -16,11 +15,14 @@ using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d1fb05e1-91d7-4dd6-932d-ac77548fedaa")]
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("fc42ce92-3119-4c6e-a006-8788c9087e39")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +31,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
