86cfb17027abbfc9f1148bb9276f7b60a6f4cb0e
Marcin Owerczuk <muzieh@gmail.com>
2013-12-18 17:15:13 +0000

    camera and torus generator

---
+++ Torus/Torus/Torus/CameraManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Torus
+{
+	public class CameraManager
+	{
+		public Matrix View { get; set; }
+		public Matrix Projection {get;set;}
+		private Vector3 _cameraPosition;
+		private Vector3 _cameraTarget;
+		private Vector3 _cameraUpVector;
+		public GraphicsDevice _graphicsDevice;
+
+		public CameraManager(GraphicsDevice graphicsDevice)
+		{
+			_graphicsDevice = graphicsDevice;
+			_cameraPosition = new Vector3(0, 0, 3f);
+			_cameraTarget = new Vector3(0, 0, 0);
+			_cameraUpVector = Vector3.UnitY;
+
+			View = Matrix.CreateLookAt(_cameraPosition, _cameraTarget, _cameraUpVector);
+			float aspectRatio = (float)graphicsDevice.Viewport.Width / (float)graphicsDevice.Viewport.Height;
+			Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 0.5f, 1000);
+		}
+
+		public Vector3 Position 
+		{
+			set
+			{
+				_cameraPosition = value;
+				View = Matrix.CreateLookAt(_cameraPosition, _cameraTarget, _cameraUpVector); 
+			}
+
+			get
+			{
+				return _cameraPosition;
+			}
+		}
+		
+	}
+}
---
+++ Torus/Torus/Torus/TorusGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Torus
+{
+	public class TorusGenerator
+	{
+		public Vector3 GetPoint(double r0, double r1, double theta, double phi)
+		{
+			double x = Math.Cos(theta) * ( r0 + r1 * Math.Cos(phi) );
+			double y = Math.Sin(theta) * ( r0 + r1 * Math.Cos(phi) );
+			double z = r1 * Math.Sin(phi);
+			return new Vector3((float)x, (float)y, (float)z);
+		}
+	}
+}
